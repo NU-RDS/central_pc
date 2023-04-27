369e5146699fc7c239bcabd71fcf59116b9d8eef
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-04-27 18:37:56 -0500

    added IK and usb2can demo
    for reading from the CAN bus
    also, added helper functions and documentation to mr_helpers.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 
 from inverse_dyn import InverseDynamics
+from inverse_kin import IKinSpace
+
 from bot_parameters import *
 import numpy as np
 
-pos = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
-Force = np.array([1, 1, 1, 1, 1, 1]) #wrench applied by the n+1 link to the outside world in its own body frame
+def iktest():
+
+    Slist_ik = np.array([[0, 0,  1,  4, 0,    0],
+                        [0, 0,  0,  0, 1,    0],
+                        [0, 0, -1, -6, 0, -0.1]]).T
+    M = np.array([[-1, 0,  0, 0],
+                    [ 0, 1,  0, 6],
+                    [ 0, 0, -1, 2],
+                    [ 0, 0,  0, 1]])
+    T = np.array([[0, 1,  0,     -5],
+                    [1, 0,  0,      4],
+                    [0, 0, -1, 1.6858],
+                    [0, 0,  0,      1]])
+    thetalist0 = np.array([1.5, 2.5, 3])
+    eomg = 0.01
+    ev = 0.001
+
+    #test of IKInSpace()
+    out = IKinSpace(Slist_ik, M, T, thetalist0, eomg, ev)
+    print(out)
+    #should be (np.array([ 1.57073783,  2.99966384,  3.1415342 ]), True)
+
+def idtest():
+    pos = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
+    Force = np.array([1, 1, 1, 1, 1, 1]) #wrench applied by the n+1 link to the outside world in its own body frame
 
 #array of joint torques produced by ID
 tor = InverseDynamics(pos, gravity, Force, Mlist, Glist, Slist)
@@ -19,3 +44,5 @@ def callpos():
   #TODO: fill in what this function represents
   pass
   return 0
+
+if __name__ == '__main__':
--- mr_helpers.py
+++ mr_helpers.py
@@ -242,3 +242,189 @@ def ad(V):
     omgmat = VecToso3([V[0], V[1], V[2]])
     return np.r_[np.c_[omgmat, np.zeros((3, 3))],
                  np.c_[VecToso3([V[3], V[4], V[5]]), omgmat]]
+
+
+def MatrixLog3(R):
+    """Computes the matrix logarithm of a rotation matrix
+
+    :param R: A 3x3 rotation matrix
+    :return: The matrix logarithm of R
+
+    Example Input:
+        R = np.array([[0, 0, 1],
+                      [1, 0, 0],
+                      [0, 1, 0]])
+    Output:
+        np.array([[          0, -1.20919958,  1.20919958],
+                  [ 1.20919958,           0, -1.20919958],
+                  [-1.20919958,  1.20919958,           0]])
+    """
+    acosinput = (np.trace(R) - 1) / 2.0
+    if acosinput >= 1:
+        return np.zeros((3, 3))
+    elif acosinput <= -1:
+        if not NearZero(1 + R[2][2]):
+            omg = (1.0 / np.sqrt(2 * (1 + R[2][2]))) \
+                  * np.array([R[0][2], R[1][2], 1 + R[2][2]])
+        elif not NearZero(1 + R[1][1]):
+            omg = (1.0 / np.sqrt(2 * (1 + R[1][1]))) \
+                  * np.array([R[0][1], 1 + R[1][1], R[2][1]])
+        else:
+            omg = (1.0 / np.sqrt(2 * (1 + R[0][0]))) \
+                  * np.array([1 + R[0][0], R[1][0], R[2][0]])
+        return VecToso3(np.pi * omg)
+    else:
+        theta = np.arccos(acosinput)
+        return theta / 2.0 / np.sin(theta) * (R - np.array(R).T)
+
+
+def se3ToVec(se3mat):
+    """ Converts an se3 matrix into a spatial velocity vector
+
+    :param se3mat: A 4x4 matrix in se3
+    :return: The spatial velocity 6-vector corresponding to se3mat
+
+    Example Input:
+        se3mat = np.array([[ 0, -3,  2, 4],
+                           [ 3,  0, -1, 5],
+                           [-2,  1,  0, 6],
+                           [ 0,  0,  0, 0]])
+    Output:
+        np.array([1, 2, 3, 4, 5, 6])
+    """
+    return np.r_[[se3mat[2][1], se3mat[0][2], se3mat[1][0]],
+                 [se3mat[0][3], se3mat[1][3], se3mat[2][3]]]
+
+
+
+def MatrixLog6(T):
+    """Computes the matrix logarithm of a homogeneous transformation matrix
+
+    :param R: A matrix in SE3
+    :return: The matrix logarithm of R
+
+    Example Input:
+        T = np.array([[1, 0,  0, 0],
+                      [0, 0, -1, 0],
+                      [0, 1,  0, 3],
+                      [0, 0,  0, 1]])
+    Output:
+        np.array([[0,          0,           0,           0]
+                  [0,          0, -1.57079633,  2.35619449]
+                  [0, 1.57079633,           0,  2.35619449]
+                  [0,          0,           0,           0]])
+    """
+    R, p = TransToRp(T)
+    omgmat = MatrixLog3(R)
+    if np.array_equal(omgmat, np.zeros((3, 3))):
+        return np.r_[np.c_[np.zeros((3, 3)),
+                           [T[0][3], T[1][3], T[2][3]]],
+                     [[0, 0, 0, 0]]]
+    else:
+        theta = np.arccos((np.trace(R) - 1) / 2.0)
+        return np.r_[np.c_[omgmat,
+                           np.dot(np.eye(3) - omgmat / 2.0 \
+                           + (1.0 / theta - 1.0 / np.tan(theta / 2.0) / 2) \
+                              * np.dot(omgmat,omgmat) / theta,[T[0][3],
+                                                               T[1][3],
+                                                               T[2][3]])],
+                     [[0, 0, 0, 0]]]
+
+
+def JacobianBody(Blist, thetalist):
+    """Computes the body Jacobian for an open chain robot
+
+    :param Blist: The joint screw axes in the end-effector frame when the
+                  manipulator is at the home position, in the format of a
+                  matrix with axes as the columns
+    :param thetalist: A list of joint coordinates
+    :return: The body Jacobian corresponding to the inputs (6xn real
+             numbers)
+
+    Example Input:
+        Blist = np.array([[0, 0, 1,   0, 0.2, 0.2],
+                          [1, 0, 0,   2,   0,   3],
+                          [0, 1, 0,   0,   2,   1],
+                          [1, 0, 0, 0.2, 0.3, 0.4]]).T
+        thetalist = np.array([0.2, 1.1, 0.1, 1.2])
+    Output:
+        np.array([[-0.04528405, 0.99500417,           0,   1]
+                  [ 0.74359313, 0.09304865,  0.36235775,   0]
+                  [-0.66709716, 0.03617541, -0.93203909,   0]
+                  [ 2.32586047,    1.66809,  0.56410831, 0.2]
+                  [-1.44321167, 2.94561275,  1.43306521, 0.3]
+                  [-2.06639565, 1.82881722, -1.58868628, 0.4]])
+    """
+    Jb = np.array(Blist).copy().astype(float)
+    T = np.eye(4)
+    for i in range(len(thetalist) - 2, -1, -1):
+        T = np.dot(T,MatrixExp6(VecTose3(np.array(Blist)[:, i + 1] \
+                                         * -thetalist[i + 1])))
+        Jb[:, i] = np.dot(Adjoint(T), np.array(Blist)[:, i])
+    return Jb
+
+def JacobianSpace(Slist, thetalist):
+    """Computes the space Jacobian for an open chain robot
+
+    :param Slist: The joint screw axes in the space frame when the
+                  manipulator is at the home position, in the format of a
+                  matrix with axes as the columns
+    :param thetalist: A list of joint coordinates
+    :return: The space Jacobian corresponding to the inputs (6xn real
+             numbers)
+
+    Example Input:
+        Slist = np.array([[0, 0, 1,   0, 0.2, 0.2],
+                          [1, 0, 0,   2,   0,   3],
+                          [0, 1, 0,   0,   2,   1],
+                          [1, 0, 0, 0.2, 0.3, 0.4]]).T
+        thetalist = np.array([0.2, 1.1, 0.1, 1.2])
+    Output:
+        np.array([[  0, 0.98006658, -0.09011564,  0.95749426]
+                  [  0, 0.19866933,   0.4445544,  0.28487557]
+                  [  1,          0,  0.89120736, -0.04528405]
+                  [  0, 1.95218638, -2.21635216, -0.51161537]
+                  [0.2, 0.43654132, -2.43712573,  2.77535713]
+                  [0.2, 2.96026613,  3.23573065,  2.22512443]])
+    """
+    Js = np.array(Slist).copy().astype(float)
+    T = np.eye(4)
+    for i in range(1, len(thetalist)):
+        T = np.dot(T, MatrixExp6(VecTose3(np.array(Slist)[:, i - 1] \
+                                * thetalist[i - 1])))
+        Js[:, i] = np.dot(Adjoint(T), np.array(Slist)[:, i])
+    return Js
+
+def FKinSpace(M, Slist, thetalist):
+    """Computes forward kinematics in the space frame for an open chain robot
+
+    :param M: The home configuration (position and orientation) of the end-
+              effector
+    :param Slist: The joint screw axes in the space frame when the
+                  manipulator is at the home position, in the format of a
+                  matrix with axes as the columns
+    :param thetalist: A list of joint coordinates
+    :return: A homogeneous transformation matrix representing the end-
+             effector frame when the joints are at the specified coordinates
+             (i.t.o Space Frame)
+
+    Example Input:
+        M = np.array([[-1, 0,  0, 0],
+                      [ 0, 1,  0, 6],
+                      [ 0, 0, -1, 2],
+                      [ 0, 0,  0, 1]])
+        Slist = np.array([[0, 0,  1,  4, 0,    0],
+                          [0, 0,  0,  0, 1,    0],
+                          [0, 0, -1, -6, 0, -0.1]]).T
+        thetalist = np.array([np.pi / 2.0, 3, np.pi])
+    Output:
+        np.array([[0, 1,  0,         -5],
+                  [1, 0,  0,          4],
+                  [0, 0, -1, 1.68584073],
+                  [0, 0,  0,          1]])
+    """
+    T = np.array(M)
+    for i in range(len(thetalist) - 1, -1, -1):
+        T = np.dot(MatrixExp6(VecTose3(np.array(Slist)[:, i] \
+                                       * thetalist[i])), T)
+    return T
